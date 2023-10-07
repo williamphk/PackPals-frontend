169906130a681d8b95b7719b41c5f16ccedb8f35
William Poon <willphk@outlook.com>
2023-10-07 19:27:33 -0400

    [Update] Register

--- src/containers/Register/Register.tsx
+++ src/containers/Register/Register.tsx
@@ -6,12 +6,12 @@ const Register: React.FC = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-    confirmPassword: "",
     first_name: "",
     last_name: "",
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.name, e.target.value);
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
@@ -28,7 +28,7 @@ const Register: React.FC = () => {
       <h1 className={styles.title}>Register</h1>
       <form onSubmit={handleSubmit} className={styles.form}>
         <div className={styles.inputGroup}>
-          <label>Email:</label>
+          <label htmlFor="email">Email:</label>
           <input
             type="email"
             name="email"
@@ -37,7 +37,7 @@ const Register: React.FC = () => {
           />
         </div>
         <div className={styles.inputGroup}>
-          <label>Password:</label>
+          <label htmlFor="password">Password:</label>
           <input
             type="password"
             name="password"
@@ -46,28 +46,19 @@ const Register: React.FC = () => {
           />
         </div>
         <div className={styles.inputGroup}>
-          <label>Confirm Password:</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-          />
-        </div>
-        <div className={styles.inputGroup}>
-          <label>First Name:</label>
+          <label htmlFor="first_name">First Name:</label>
           <input
             type="text"
-            name="firstName"
+            name="first_name"
             value={formData.first_name}
             onChange={handleChange}
           />
         </div>
         <div className={styles.inputGroup}>
-          <label>Last Name:</label>
+          <label htmlFor="last_name">Last Name:</label>
           <input
             type="text"
-            name="lastName"
+            name="last_name"
             value={formData.last_name}
             onChange={handleChange}
           />
