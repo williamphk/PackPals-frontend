59f8e7d3c96660e7cc96f5c75aa83ae19c6e8d4e
William Poon <willphk@outlook.com>
2023-10-07 16:27:38 -0400

    [Update] Match Interface

--- src/models/Match.ts
+++ src/models/Match.ts
@@ -1,12 +1,14 @@
 export interface Match {
   id: string;
   product_name: string;
+  requesterId: string;
   requesterDetails?: {
     first_name: string;
     last_name: string;
     email: string;
     created_date: Date;
   };
+  requesteeId: string;
   requesteeDetails?: {
     first_name: string;
     last_name: string;
