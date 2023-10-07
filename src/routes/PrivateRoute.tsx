f63639e8f329e4c8baa430933e7d4fe21c39e18d
William Poon <willphk@outlook.com>
2023-10-07 16:17:23 -0400

    [Create] PrivateRoute

---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, useNavigate } from "react-router-dom";
+import { useUser } from "../context/UserContext";
+
+interface PublicRouteProps {
+  path: string;
+  element: React.ReactElement;
+}
+
+const PublicRoute: React.FC<PublicRouteProps> = ({ element, path }) => {
+  let { user } = useUser();
+  let navigate = useNavigate();
+
+  if (user) {
+    return <Route path={path} element={element} />;
+  }
+
+  navigate("/login");
+};
+
+export default PublicRoute;
