b1ad293f9d030d55d19eff902789d5c9af2a8e52
William Poon <willphk@outlook.com>
2023-10-07 21:47:28 -0400

    [Implement] Logout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import { useUser } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
+  const { isAuthenticated, setUser } = useUser();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    localStorage.removeItem("refreshToken");
+    setUser(null);
+    navigate("/login");
+  };
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -10,12 +23,18 @@ const Header: React.FC = () => {
           <div className={styles.logo}>Logo</div>
         </Link>
         <div className={styles.authButtons}>
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
-          <Link to="/register">
-            <button>Register</button>
-          </Link>
+          {isAuthenticated ? (
+            <button onClick={handleLogout}>Logout</button>
+          ) : (
+            <div>
+              <Link to="/login">
+                <button>Login</button>
+              </Link>
+              <Link to="/register">
+                <button>Register</button>
+              </Link>
+            </div>
+          )}
         </div>
       </header>
     </div>
