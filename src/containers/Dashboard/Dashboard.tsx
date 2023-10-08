1880cffef29cd908aaf3140c843c4aef91fefe44
William Poon <willphk@outlook.com>
2023-10-08 15:30:39 -0400

    [Update] Dashboard

--- src/containers/Dashboard/Dashboard.tsx
+++ src/containers/Dashboard/Dashboard.tsx
@@ -18,12 +18,38 @@ const Dashboard: React.FC = () => {
 
       <section className={styles.matchesSection}>
         <div className={styles.matchCategory}>
-          <h3>Newly Matched</h3>
+          <h3>Recent Matches</h3>
           <ul>
-            <li>User name</li>
-            <li>User name</li>
-            <li>User name</li>
-            <li>User name</li>
+            <li>User name Product name</li>
+            <li>User name Product name</li>
+            <li>User name Product name</li>
+            <li>User name Product name</li>
+          </ul>
+          <button>See more</button>
+        </div>
+      </section>
+
+      <section className={styles.matchesSection}>
+        <div className={styles.matchCategory}>
+          <h3>Ongoing Matches</h3>
+          <ul>
+            <li>Product name</li>
+            <li>Product name</li>
+            <li>Product name</li>
+            <li>Product name</li>
+          </ul>
+          <button>See more</button>
+        </div>
+      </section>
+
+      <section className={styles.matchesSection}>
+        <div className={styles.matchCategory}>
+          <h3>You Might Like</h3>
+          <ul>
+            <li>User name Product name</li>
+            <li>User name Product name</li>
+            <li>User name Product name</li>
+            <li>User name Product name</li>
           </ul>
           <button>See more</button>
         </div>
