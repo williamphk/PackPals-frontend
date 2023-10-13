c8f27852f8a079a2a4bc50ed70cf4797022b18c3
William Poon <willphk@outlook.com>
2023-10-13 16:39:48 -0400

    [Update] Register Style

--- src/containers/Register/Register.tsx
+++ src/containers/Register/Register.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import styles from "./Register.module.css";
 import { register } from "../../services/auth";
 import { useNavigate } from "react-router-dom";
 
@@ -27,46 +26,67 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>Register</h2>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.inputGroup}>
-          <label htmlFor="email">Email:</label>
+    <div className="flex flex-col items-center justify-center h-full p-4">
+      <h2 className="text-2xl font-bold mb-6">Register</h2>
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-6 rounded-xl shadow-md w-full max-w-md"
+      >
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-2" htmlFor="email">
+            Email*
+          </label>
           <input
             type="email"
             name="email"
             value={formData.email}
             onChange={handleChange}
+            className="p-2 w-full border rounded focus:border-blue-500 focus:outline-none"
           />
         </div>
-        <div className={styles.inputGroup}>
-          <label htmlFor="password">Password:</label>
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-2" htmlFor="password">
+            Password*
+          </label>
           <input
             type="password"
             name="password"
             value={formData.password}
             onChange={handleChange}
+            className="p-2 w-full border rounded focus:border-blue-500 focus:outline-none"
           />
         </div>
-        <div className={styles.inputGroup}>
-          <label htmlFor="first_name">First Name:</label>
+        <div className="mb-4">
+          <label
+            className="block text-sm font-medium mb-2"
+            htmlFor="first_name"
+          >
+            First Name*
+          </label>
           <input
             type="text"
             name="first_name"
             value={formData.first_name}
             onChange={handleChange}
+            className="p-2 w-full border rounded focus:border-blue-500 focus:outline-none"
           />
         </div>
-        <div className={styles.inputGroup}>
-          <label htmlFor="last_name">Last Name:</label>
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-2" htmlFor="last_name">
+            Last Name*
+          </label>
           <input
             type="text"
             name="last_name"
             value={formData.last_name}
             onChange={handleChange}
+            className="p-2 w-full border rounded focus:border-blue-500 focus:outline-none"
           />
         </div>
-        <button type="submit" className={styles.registerButton}>
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+        >
           Register
         </button>
       </form>
